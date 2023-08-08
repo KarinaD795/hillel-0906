76915e7604b1bab113b15ae41709520a7c88c720
autodoc <alexeym@pkwteile.de>
2023-08-08 19:37:04 +0300

    new hm_16

--- src/ua/hillel/homeworks/hm16/Main.java
+++ src/ua/hillel/homeworks/hm16/Main.java
@@ -2,6 +2,6 @@ package ua.hillel.homeworks.hm16;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("HomeWork for Leha");
+        System.out.println(" for Leha");
     }
 }
